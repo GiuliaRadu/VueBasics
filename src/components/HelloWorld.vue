01e425df3ee263548a5150f23eb555520266d5dd
GiuliaRadu <radugiulia17@gmail.com>
2021-05-04 12:11:13 +0300

    Made on click event listener

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button v-on:click="reverse">Reverse message</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods:{
+    reverse()
+    {
+      this.msg = this.msg.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
